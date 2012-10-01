6097fbb92454e7d8ebc102a92f8377019a204a20
Alex Gosse <gosse.alex@gmail.com>
2012-10-01 15:40:03 +0100

    Adding health monitor LWRP for RS support.

---
+++ cookbooks/stingray/resources/monitor.rb
@@ -0,0 +1,12 @@
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :can_use_ssl, :kind_of => String, :default => "Yes"
+attribute :delay, :kind_of => Integer, :default => 5
+attribute :editable_keys, :kind_of => [ String, Array ]
+attribute :factory, :kind_of => String, :default => "No"
+attribute :failures, :kind_of => Integer, :default => 3
+attribute :scope, :kind_of => Integer, :default => "pernode"
+attribute :timeout, :kind_of => Integer, :default => 10
+attribute :type, :kind_of => String
+attribute :use_ssl, :kind_of => String, :default => "No"
+
+actions :configure, :delete
