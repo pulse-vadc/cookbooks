47e014719cf3c1462e176b0292cf84a9e58409fb
Alex Gosse <gosse.alex@gmail.com>
2012-10-02 11:06:39 +0100

    Implemented monitor LWRP.

--- cookbooks/stingray/providers/monitor.rb
+++ cookbooks/stingray/providers/monitor.rb
@@ -9,11 +9,15 @@ action :configure do
         source "monitor.erb"
         mode "0644"
             variables(
-                :enabled => defined?(nr.enabled).class != NilClass ? nr.enabled : cr.enabled,
-                :ipaddresses => defined?(nr.ipaddresses).class != NilClass ? nr.ipaddresses : cr.ipaddresses,
-                :keeptogether => defined?(nr.keeptogether).class != NilClass ? nr.keeptogether : cr.keeptogether,
-                :machines => defined?(nr.machines).class != NilClass ? nr.machines : cr.machines,
-                :slaves => defined?(nr.slaves).class != NilClass ? nr.slaves : cr.slaves
+                :can_use_ssl => defined?(nr.can_use_ssl).class != NilClass ? nr.can_use_ssl : cr.can_use_ssl,
+                :delay => defined?(nr.delay).class != NilClass ? nr.delay : cr.delay,
+                :editable_keys => defined?(nr.editable_keys).class != NilClass ? nr.editable_keys : cr.editable_keys,
+                :factory => defined?(nr.factory).class != NilClass ? nr.factory : cr.factory,
+                :failures => defined?(nr.failures).class != NilClass ? nr.failures : cr.failures,
+                :scope => defined?(nr.scope).class != NilClass ? nr.scope : cr.scope,
+                :timeout => defined?(nr.timeout).class != NilClass ? nr.timeout : cr.timeout,
+                :type => defined?(nr.type).class != NilClass ? nr.type : cr.type,
+                :use_ssl => defined?(nr.use_ssl).class != NilClass ? nr.use_ssl : cr.use_ssl
                 )
 	end
 
@@ -21,7 +25,7 @@ end
 
 action :delete do
 
-	log "Deleting Traffic IP Group #{new_resource.name}"
+	log "Deleting monitor #{new_resource.name}"
 	file "#{new_resource.name}" do
 		action :delete
 	end
