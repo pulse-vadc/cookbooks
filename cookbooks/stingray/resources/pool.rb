ede998b00757fffffe3ae45bd516ebe387982bc6
Alex Gosse <gosse.alex@gmail.com>
2012-10-03 15:09:35 +0100

    Added default value for algorithm attribute.

--- cookbooks/stingray/resources/pool.rb
+++ cookbooks/stingray/resources/pool.rb
@@ -6,7 +6,7 @@ attribute :monitors, :kind_of => [String, Array]
 attribute :maxconns, :kind_of => Integer
 attribute :persistence, :kind_of => String
 attribute :weightings, :kind_of => Hash
-attribute :algorithm, :kind_of => String
+attribute :algorithm, :kind_of => String, default => "roundrobin"
 attribute :priority, :kind_of => [String, Array]
 
 actions :configure, :delete
